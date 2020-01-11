2ea133a24b8ee7064350196f7b59b0c3f483e200
lechatthecat <shu845@gmail.com>
2020-01-12 01:44:36 +0900

    add

--- docker/laravel/laravel_build.sh
+++ docker/laravel/laravel_build.sh
@@ -1,7 +1,5 @@
 #!/bin/ash
-cd /laravel
 composer self-update
-composer create-project --prefer-dist laravel/laravel sample_laravel
 cd /laravel/sample_laravel
 php artisan key:generate
 chown -R www-data:www-data .
