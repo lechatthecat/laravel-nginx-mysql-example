32cd1ba76af479ce36b2b6fb13413fc2d2bdb581
lechatthecat <shu845@gmail.com>
2020-11-12 06:38:34 +0900

    change bash

--- docker/laravel/laravel_build.sh
+++ docker/laravel/laravel_build.sh
@@ -2,8 +2,11 @@
 composer self-update
 composer create-project --prefer-dist laravel/laravel sample_laravel
 cd /laravel/sample_laravel
-php artisan key:generate
 chown -R www-data:www-data .
 chmod 777 storage
 npm install
 npm run dev 
+composer update
+composer self-update
+composer install
+php artisan key:generate
